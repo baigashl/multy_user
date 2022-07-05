bba64492ef8d49fd7c6fd78be9190d68bee1f42a
Din <dinstamaly@gmail.com>
2022-07-05 09:24:36 +0600

    price to admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from apps.accounts.models import (
-    Account, Image, Category, Amenity
+    Account, Image, Category, Amenity, Price
 )
 admin.site.register(Category)
 admin.site.register(Account)
 admin.site.register(Image)
 admin.site.register(Amenity)
+admin.site.register(Price)
